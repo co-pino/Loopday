4115da95ed5d6b2218b0e4c09f7b41db4e2ee84a
Constanza Raquel Pino Villa <co.pinov@duocuc.cl>
2026-04-19 12:54:01 -0400

    chore: resuelvo conflicto de proyecto dentro de proyecto

---
+++ constants/colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+  primary: '#000000',
+  background: '#FFFFFF',
+  textPrimary: '#000000',
+  textSecondary: '#666666',
+  success: '#1D9E75',
+  danger: '#E24B4A',
+  border: '#E5E5E5',
+}
